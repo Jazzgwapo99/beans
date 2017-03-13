e090e43a667138b7a294720ca343c630b37c817b
SkeltonMod <abgaoe@gmail.com>
2017-03-13 12:28:00 +0800

    Add files via upload

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CISCO 01
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+//display the results 
+JOptionPane.showMessageDialog ( 
+null, "Welcom to AMA Computer College \n You are Now officially a Grade 11 completer \n See you soon this June 2017"); 
+System.exit ( 0 ); //ends the program 
+    }
+}
